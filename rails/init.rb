2507e9c919a370a20bd2ba5fc9d74ac74a7b3bbe
Marty Smyth <martian@midgard.org>
2010-01-02 16:56:46 -0800

    Removing pointless spammy log line

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,3 @@
 require File.join(File.dirname(__FILE__), *%w[.. lib timing_logger])
 
 ActionController::Base.send :include, TimingLogger::Controller
-
-RAILS_DEFAULT_LOGGER.info("** TimingLogger: initialized properly")
